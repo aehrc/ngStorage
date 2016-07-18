3a0f52869fd1be470218c1b88ffd082b21751b09
Michael Lawley <michael.lawley@csiro.au>
2016-07-18 16:43:23 +1000

    apply equivalent of 2336227664e9b8bc72f0e21bb533948a4c489604 and subsequent fixups

--- ngStorage.js
+++ ngStorage.js
@@ -42,12 +42,14 @@
             '$window',
             '$log',
             '$timeout',
+            '$document',
 
             function(
                 $rootScope,
                 $window,
                 $log,
-                $timeout
+                $timeout,
+                $document
             ){
                 function isStorageSupported(storageType) {
 
@@ -143,7 +145,10 @@
 
                 // #6: Use `$window.addEventListener` instead of `angular.element` to avoid the jQuery-specific `event.originalEvent`
                 'localStorage' === storageType && $window.addEventListener && $window.addEventListener('storage', function(event) {
-                    if ('ngStorage-' === event.key.slice(0, 10)) {
+                    // Reference doc.
+                    var doc = $document[0];
+
+                    if ( (!doc.hasFocus || !doc.hasFocus()) && 'ngStorage-' === event.key.slice(0, 10) ) {
                         event.newValue ? $storage[event.key.slice(10)] = angular.fromJson(event.newValue) : delete $storage[event.key.slice(10)];
 
                         _last$storage = angular.copy($storage);
